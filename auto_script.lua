2637f72becf1a56c8c6efbc7bf2a90c5d16177c6
AkumajouHelp <122201497+AkumajouHelp@users.noreply.github.com>
2025-05-02 08:14:19 +0800

    Update auto_script.lua

--- auto_script.lua
+++ auto_script.lua
@@ -23,9 +23,6 @@
 Westbound Auto Farm Script (Enhanced Anti-Cheat and Optimized Performance)
 Author: AkumajouHelp
 
-My Main Script Link Page:
-https://github.com/AkumajouHelp/westbound-script-auto-farm
-
 Features:
 - Auto farm coyotes
 - Auto sell when inventory full
